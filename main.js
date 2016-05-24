8e018404936fd2ffd53a8815de7eab49aaefba79
Kyle Luck <luck.kyle@gmail.com>
2016-05-24 15:04:42 -0400

    adding initial files

---
+++ main.js
@@ -0,0 +1,25 @@
+function getData(callback) {
+  $.ajax({
+    method: 'GET',
+    headers: {
+    },
+    url: 'https://api.github.com/emojis',
+    success: function(data) {
+      callback(data);
+    }
+  });
+}
+
+$(function() {
+  getData(function(result) {
+    var images;
+    for (var prop in result) {
+      if (result.hasOwnProperty(prop)) {
+        images += "<img src='" + result[prop] + "' alt='" + prop + "'>";
+      }
+      //console.log(result[prop]);
+    }
+    $('.container').append(images);
+    console.log(result);
+  });
+});
