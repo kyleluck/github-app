e94d9b93258df9406333b8a8603cc3c47e54394f
Kyle Luck <luck.kyle@gmail.com>
2016-05-24 15:54:57 -0400

    styling improvements

--- followers.js
+++ followers.js
@@ -1,6 +1,7 @@
-function getData(callback) {
+function getFollowers(callback) {
   $.ajax({
     method: 'GET',
+    headers: {"Authorization": "Basic YWlycG9ydHloOmZ1enp5bG9naWM="},
     url: 'https://api.github.com/users/kyleluck/followers',
     success: function(data) {
       callback(data);
@@ -8,13 +9,33 @@ function getData(callback) {
   });
 }
 
+function searchCode(callback) {
+  $.ajax({
+    method: 'GET',
+    headers: {"Authorization": "Basic YWlycG9ydHloOmZ1enp5bG9naWM="},
+    url: 'https://api.github.com/search/code?q=addClass+in:file+language:js+user:kyleluck',
+    success: function(data) {
+      callback(data);
+    }
+  })
+}
+
 $(function() {
-  getData(function(result) {
+  getFollowers(function(result) {
     var avatars = '';
     result.forEach(function(obj) {
       avatars += "<a href='" + obj.html_url + "'><img src='" + obj.avatar_url + "' alt='" + obj.login + "'></a>";
     });
     $('.followers').append(avatars);
+    //console.log(result);
+  });
+
+  searchCode(function(result) {
+    var codeResults = '';
+    for (var i = 0; i < result.items.length; i++) {
+      codeResults += "<p>" + result.items[i].url + "</p>";
+    }
     console.log(result);
+    $('.code').append(codeResults);
   });
 });
